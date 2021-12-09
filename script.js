d4f8b0492338c0a1897c9162742ebec993a88625
peti <rootkit507@gmail.com>
2021-12-09 12:39:00 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuIcon = document.querySelector(".fas");
+let menu = document.querySelector(".menu");
+
+menuIcon.addEventListener("click", () => {
+    if (menuIcon.classList.contains("show")){
+        menu.style.top = `-100%`;
+        menuIcon.classList.remove("show");
+    }else{
+         menu.style.top = `0px`;
+         menuIcon.classList.add("show");
+    }
+})
